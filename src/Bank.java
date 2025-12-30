2b821b5e840d3590ef993895ec0ca97fe09fb97b
nicolas.martina69@gmail.com <nicolas.martina69@gmaill.como>
2025-12-30 00:03:20 -0300

    Created the Bank class and a Main Menu in the Menu class.

--- src/Bank.java
+++ src/Bank.java
@@ -1,2 +1,164 @@
+import java.util.ArrayList;
+
 public class Bank {
+    ArrayList<Account> accounts = new ArrayList<>();
+
+    public void createAccount(String accountName, double balance){
+        Account newAccount = new Account(accountName, balance);
+        accounts.add(newAccount);
+        System.out.println("Account added successfully");
+    }
+
+    public void deleteAccount(String accountName){
+        if (accounts.isEmpty()){
+            System.out.println("There are no accounts to delete.");
+            return;
+        }
+        
+        boolean found = false;
+        for (int i = accounts.size() - 1; i >= 0; i--) {
+            Account a = accounts.get(i);
+
+            if (a.getAccountName().equalsIgnoreCase(accountName)){
+                accounts.remove(i);
+                System.out.println("Account from " + " has been deleted successfully.");
+                System.out.println("---------");
+                found = true;
+            }
+        }
+
+
+        if (!found){
+            System.out.println("The account you are looking for does not exist.");
+        }
+    }
+
+
+    public void displayAccounts(){
+        for (Account a : accounts){
+            System.out.println("Account: " + a.getAccountName());
+            System.out.println("Balance: " + a.getBalance());
+            System.out.println("---------");
+        }
+    }
+
+    public void searchAccount(String accountName){
+        if (accounts.isEmpty()){
+            System.out.println("No accounts with that name exist.");
+            return;
+        }
+        boolean found = false;
+        for (Account a : accounts){
+            if (a.getAccountName().equalsIgnoreCase(accountName)){
+                System.out.println("Account: " + a.getAccountName());
+                System.out.println("Balance: " + a.getBalance());
+                System.out.println("---------");
+                System.out.println();
+                found = true;
+            }
+        }
+
+        if (!found){
+            System.out.println("The account with name " + accountName + " doesn't exist");
+            System.out.println("-----");
+            System.out.println();
+        }
+    }
+
+
+    public void depositToAccount(String accountName, double balance){
+        if (accounts.isEmpty()){
+            System.out.println("The account doesn't exist");
+            return;
+        }
+
+        boolean found = false;
+        for (Account a : accounts){
+            if (a.getAccountName().equalsIgnoreCase(accountName)){
+                a.deposit(balance);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found){
+            System.out.println("Account not found.");
+        }
+    }
+
+    public void withdrawFromAccount(String accountName, double amount){
+        if (accounts.isEmpty()){
+            System.out.println("The account doesn't exist");
+            return;
+        }
+
+        boolean found = false;
+        for (Account a : accounts){
+            if (a.getAccountName().equalsIgnoreCase(accountName)){
+                a.withdraw(amount);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Account not found.");
+        }
+    }
+
+    public void transferBetweenAccounts(String fromAccountName, String toAccountName, double amount){
+        if (accounts.isEmpty()){
+            System.out.println("The account you are looking for doesn't exist");
+            return;
+        }
+
+        if (fromAccountName.equalsIgnoreCase(toAccountName)){
+            System.out.println("Cannot transfer to same account.");
+            return;
+        }
+
+
+        Account fromAccount = null;
+        for (Account a : accounts){
+            if (a.getAccountName().equalsIgnoreCase(fromAccountName)){
+               fromAccount = a;
+               break;
+            }
+        }
+
+        Account toAccount = null;
+        for (Account a : accounts){
+            if (a.getAccountName().equalsIgnoreCase(toAccountName)){
+                toAccount = a;
+                break;
+            }
+        }
+
+        if (fromAccount == null) {
+            System.out.println("The source was not found");
+            return;
+        }
+
+        if (toAccount == null) {
+            System.out.println("The source was not found");
+            return;
+        }
+
+        if (fromAccount.getBalance() < amount){
+            System.out.println("Not enough money on " + fromAccountName);
+            return;
+        }
+
+        fromAccount.withdraw(amount);
+        toAccount.deposit(amount);
+        System.out.println("Transfered $ " + amount + " from account " + fromAccountName + "to account " + toAccountName);
+
+
+    }
+
+
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,84 @@
-import java.util.ArrayList;
+
 import java.util.Scanner;
-import java.util.Random;
+
 
 public class Main {
 
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int option = 0;
+        Bank bank = new Bank();
 
+        while (option != 8){
+
+            System.out.println(" ==== Bank Management System ====");
+            System.out.println("1. Create Account");
+            System.out.println("2. Delete Account");
+            System.out.println("3. Show All Accounts");
+            System.out.println("4. Search an Account");
+            System.out.println("5. Deposit");
+            System.out.println("6. Withdraw");
+            System.out.println("7. Transfer");
+            System.out.println("8. Exit");
+            System.out.println();
+            System.out.println("Enter choice: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option){
+                case 1:
+                    System.out.println("Enter the account name: ");
+                    String accountName = scanner.nextLine();
+                    System.out.println("Enter a starting balance: ");
+                    double balance = scanner.nextDouble();
+                    bank.createAccount(accountName, balance);
+                    break;
+                case 2:
+                    System.out.println("Enter name of account you want to delete: ");
+                    String deleteAccount = scanner.nextLine();
+                    bank.deleteAccount(deleteAccount);
+                    break;
+                case 3:
+                    bank.displayAccounts();
+                    break;
+                case 4:
+                    System.out.println("Enter name of the account: ");
+                    String searchAccount = scanner.nextLine();
+                    bank.searchAccount(searchAccount);
+                    break;
+                case 5:
+                    System.out.println("Enter name of the account: ");
+                    String name = scanner.nextLine();
+                    System.out.println("Enter amount: ");
+                    double amount = scanner.nextDouble();
+                    bank.depositToAccount(name, amount);
+                    break;
+                case 6:
+                    System.out.println("Enter name of the account: ");
+                    String withdrawalAccount = scanner.nextLine();
+                    System.out.println("Enter amount: ");
+                    double amountToWithdraw = scanner.nextDouble();
+                    bank.withdrawFromAccount(withdrawalAccount, amountToWithdraw);
+                    break;
+                case 7:
+                    System.out.println("Enter provider account: ");
+                    String fromAccount = scanner.nextLine();
+                    System.out.println("Enter receiver account: ");
+                    String toAccount = scanner.nextLine();
+                    System.out.println("Enter amount to transfer: ");
+                    double amountToTransfer = scanner.nextDouble();
+                    bank.transferBetweenAccounts(fromAccount, toAccount,amountToTransfer);
+                    break;
+                case 8:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("invalid choice, please try again.");
+
+            }
+        }
+
+        scanner.close();
 
 
 
